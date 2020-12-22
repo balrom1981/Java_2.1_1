b3ab7dfc3c2243408f19bdfb5e3522e3fb4263c2
balrom1981 <balrom@bk.ru>
2020-12-22 15:26:59 +0300

    Application made

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 15_000;
+        int mile = 20;
+        int bonus = amount / mile;
+        System.out.print(bonus);
+    }
+}
